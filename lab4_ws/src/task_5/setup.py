786bed66382e47ce9265fc5a86b6a5b629e60f60
Aryaman Akolkar <aryamanakolkar11@gmail.com>
2026-01-13 22:36:20 -0500

    Adding lab4_ws

---
+++ lab4_ws/src/task_5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'task_5'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='me597',
+    maintainer_email='aryamanakolkar11@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'image_publisher = task_5.image_publisher:main',
+        'object_detector = task_5.object_detector:main',
+        ],
+    },
+)
